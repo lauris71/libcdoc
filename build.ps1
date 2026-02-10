6aab6605314758ed7db363a03961746558f4a3f6
Lauris Kaplinski <lauris@raulwalter.com>
2026-02-10 16:46:48 +0200

    Set msi_name

--- build.ps1
+++ build.ps1
@@ -3,6 +3,8 @@ param(
   [string]$libcdoc = $PSScriptRoot,
   [string]$platform = "x64",
   [string]$build_number = $(if ($null -eq $env:BUILD_NUMBER) {"0"} else {$env:BUILD_NUMBER}),
+  [string]$msiversion = "1.0.0.$build_number",
+  [string]$msi_name = "libcdoc-$msiversion$env:VER_SUFFIX.$platform.msi",
   [string]$git = "git.exe",
   [string]$vcpkg = "vcpkg\vcpkg.exe",
   [string]$vcpkg_dir = (split-path -parent $vcpkg),
